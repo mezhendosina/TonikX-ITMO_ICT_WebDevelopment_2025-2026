387b4f16dd8abf7314cd273485b583b97320d967
mezhendosina <mensh.zheya@gmail.com>
2025-09-02 23:32:59 +0300

    add mathematics to lab1.2

--- students/k3341/Menshenin_Evgenii/Lr1/2/mathematics.py
+++ students/k3341/Menshenin_Evgenii/Lr1/2/mathematics.py
@@ -5,15 +5,16 @@ import dataclasses
 class Math:
     a: int
     b: int
+    h: int
 
     def do_math(self) -> float:
-        return self.a * self.b
+        return (self.a + self.b) / self.h
 
 
 def parse_math(string: str) -> Math:
     split_string = string.split(",")
-    return Math(int(split_string[0]), int(split_string[1]))
+    return Math(int(split_string[0]), int(split_string[1]), int(split_string[2]))
 
 
 def decode_math(math: Math) -> str:
-    return f"{math.a},{math.b}"
+    return f"{math.a},{math.b},{math.h}"
